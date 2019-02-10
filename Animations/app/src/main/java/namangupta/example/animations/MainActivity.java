757b6b5f80fb0c2189d3faf6edad0ccb34a9e097
naman1399 <naman.gupta1399@gmail.com>
2019-02-10 22:04:06 +0530

    new commit

--- Animations/app/src/main/java/namangupta/example/animations/MainActivity.java
+++ Animations/app/src/main/java/namangupta/example/animations/MainActivity.java
@@ -10,10 +10,16 @@ public class MainActivity extends AppCompatActivity {
     public void fade(View view) {
 
         ImageView bart = (ImageView) findViewById(R.id.bart);
-        ImageView homer = (ImageView) findViewById(R.id.homer);
 
-        bart.animate().alpha(0f).setDuration(2000);
-        homer.animate().alpha(1f).setDuration(2000);
+        bart.animate()
+                .translationXBy(1000f)
+                .translationYBy(1000f)
+                .rotationBy(3600)
+                .setDuration(2000);
+
+//        ImageView homer = (ImageView) findViewById(R.id.homer);
+
+//        homer.animate().alpha(1f).setDuration(2000);
 
     }
 
@@ -21,5 +27,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView bart = (ImageView) findViewById(R.id.bart);
+
+        bart.setTranslationX(-1000f);
+        bart.setTranslationY(-1000f);
     }
 }
